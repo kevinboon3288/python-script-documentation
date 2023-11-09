2c8baaf3a885fb2a19d254f221df6570a7a82965
Kevin Boon <kevintan7227@gmail.com>
2023-11-09 10:29:57 +0800

    Add delete and create function for file handler

--- file-handling/file-handler.py
+++ file-handling/file-handler.py
@@ -2,34 +2,53 @@ import os
 import shutil
 import argparse
 
+def create(targetPath):
+    if not os.path.exists(targetPath):
+        with open(targetPath, 'w') as file:
+            file.write('')
+        if os.path.exists(targetPath):
+            print(f"{targetPath} created successfully")
+        else:
+            print(f"{targetPath} not created successfully")
+        
+def delete(targetPath):
+    if os.path.exists(targetPath):
+        os.remove(targetPath)
+        if not os.path.exists(targetPath):
+            print(f"{targetPath} deleted successfully")
+        else:
+            print(f"{targetPath} not deleted successfully")
+
 def copy(sourcePath, destinationPath):
     if not os.path.exists(sourcePath):
-        print(f"[Not Exist] filename is {sourcePath}")
+        print(f"filename is {sourcePath}")
     else:
-        createDirectory(destinationPath)
+        makeDirectory(destinationPath)
         shutil.copy(sourcePath, destinationPath)
 
-def createDirectory(targetPath):
+def makeDirectory(targetPath):
     if not os.path.exists(targetPath):
         os.makedirs(targetPath)
-    else:
-        print(f"Directory path already exist : {targetPath}")
 
 def arg_Parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--function', help="use --function | Functions in File Handling Feature (eg. copy)")
-    parser.add_argument('--source', help="use --sourcePath | Set the source path if any")
-    parser.add_argument('--dest', help="use --destPath | Set the destination path if any")
+    parser.add_argument('--f', help="use --f | Functions in File Handling Feature (eg. copy)")
+    parser.add_argument('--s', help="use --s | Set the source path if any")
+    parser.add_argument('--d', help="use --d | Set the destination path if any")
 
     return parser.parse_args()
 
 def main():
     args = arg_Parse()
 
-    if args.function == "create":
-        createDirectory(args.source)
-    elif args.function == "copy":
-        copy(args.source, args.dest)
+    if args.f == "directory":
+        makeDirectory(args.s)
+    elif args.f == "create":
+        create(args.s)
+    elif args.f == "delete":
+        delete(args.s)
+    elif args.f == "copy":
+        copy(args.s, args.d)
 
 if __name__ == "__main__":
     main()
